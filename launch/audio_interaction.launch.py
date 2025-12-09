e7049b68645818d18e0e20a47af7d64d1eb92b0d
wunuo1 <847005649@qq.com>
2025-12-09 21:29:13 +0800

    Init MagicBox version

---
+++ launch/audio_interaction.launch.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2024，D-Robotics.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python import get_package_share_directory
+
+
+def generate_launch_description():
+    audio_node = Node(
+        package='audio_io',
+        executable='audio_io',
+        output='screen',
+        parameters=[
+            {"tts_config_path": "/userdata/MagicBox/dep/matcha-icefall-zh-baker",
+            "asr_model_path": "/userdata/MagicBox/config/"},
+        ],
+        arguments=['--ros-args', '--log-level', 'warn']
+    )
+
+    hobot_llamacpp_node = Node(
+        package='my_hobot_llamacpp',
+        executable='my_hobot_llamacpp',
+        output='screen',
+        parameters=[
+            {"feed_type": 2},
+            {"llm_model_name": "/dev/shm/qwen2.5-1.5b-instruct-q5_k_m.gguf"},
+            {"system_prompt": "config/system_prompt.txt"},
+            {"cute_words": "你好，请问有什么能够帮助您的？"}
+        ],
+        arguments=['--ros-args', '--log-level', 'warn']
+    )
+
+
+    return LaunchDescription([
+        # audio_node,
+        hobot_llamacpp_node
+    ])
